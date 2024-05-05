f8f9c5545304fe553ceff31385422abd834c9c9d
Stephen Olayemi <olayemistephen007@gmail.com>
2024-05-05 09:46:53 +0100

    work in progress

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string name, List<string>categories, string description, string imageFile, decimal price) 
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product entity from incoming command object
+            var product = new Product
+            {
+                Name = command.name, 
+                Description = command.description,
+                Categories = command.categories,
+                Price = command.price,
+                ImageFile = command.imageFile,
+            };
+
+            //save to database
+
+            //return result - CreateProductResult
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,10 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the conatianer
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
-
 //configure HTTP Request pipeline
+app.MapCarter();
 
 app.Run();
