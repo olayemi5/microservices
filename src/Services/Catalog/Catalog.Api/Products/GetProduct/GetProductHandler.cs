72009fdc7ca4ff9466e97a176ce481538f16e015
Stephen Olayemi <olayemistephen007@gmail.com>
2024-05-05 20:18:17 +0100

    get product endpoint created

---
+++ src/Services/Catalog/Catalog.Api/Products/GetProduct/GetProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.Api.Products.CreateProduct;
+
+namespace Catalog.Api.Products.GetProduct
+{
+    public record GetProductsQuery()
+       : IQuery<GetProductResult>;
+
+    public record GetProductResult(IEnumerable<Product> Products);
+
+    internal class GetProductQueryHandler(IDocumentSession session, ILogger<GetProductQueryHandler> logger)
+       : IQueryHandler<GetProductsQuery, GetProductResult>
+    {
+        public async Task<GetProductResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductQueryHandler.Handle called with query {@query}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductResult(products);
+        }
+    }
+}
