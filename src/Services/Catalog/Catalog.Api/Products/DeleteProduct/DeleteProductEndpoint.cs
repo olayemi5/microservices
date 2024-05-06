549ade5ed64f0e97cff74b447c69bc9f85de71ac
Stephen Olayemi <olayemistephen007@gmail.com>
2024-05-06 18:56:17 +0100

    delete product api completed

---
+++ src/Services/Catalog/Catalog.Api/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.Api.Products.DeleteProduct
+{
+    public record DeleteProductRequest(Guid Id);
+    public record DeleteProductResponse(bool IsSuccess);
+
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/product/{id}", 
+                async (Guid id, ISender sender) =>
+                {
+                    var result = await sender.Send(new DeleteRecordCommand(id));
+                    var response = result.Adapt<DeleteProductResponse>();
+
+                    return Results.Ok(response);
+                })
+              .WithName("DeleteProducts")
+             .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .WithSummary("Delete Products")
+            . WithDescription("Delete Products");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/DeleteProduct/DeleteProducthandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Catalog.Api.Products.DeleteProduct
+{
+    public record DeleteRecordCommand(Guid Id) : ICommand<DeleteRecordResult>;
+
+    public record DeleteRecordResult(bool IsSuccess);
+
+    internal class DeleteProducthandler(IDocumentSession session, ILogger<DeleteProducthandler> logger)
+        : ICommandHandler<DeleteRecordCommand, DeleteRecordResult>
+    {
+        public async Task<DeleteRecordResult> Handle(DeleteRecordCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Delete product with id {command.Id}");
+
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteRecordResult(true);
+        }
+    }
+}
