b42beea3fb0de3de1f3fd2b3d232ecb1c8e24ab4
Stephen Olayemi <olayemistephen007@gmail.com>
2024-05-10 08:26:55 +0100

    work in progress

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,20 +1,47 @@
 ﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
 using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace Discount.Grpc.Services;
 
 public class DiscountService
-    (DiscountContext context, ILogger<DiscountService> logger) 
+    (DiscountContext dbContext, ILogger<DiscountService> logger) 
     : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon == null) 
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+        logger.LogInformation($"Reponse retrieved for product name - {request.ProductName} - {JsonSerializer.Serialize(coupon)}");
+
+        var couponModel = coupon.Adapt<CouponModel>();
+
+        return couponModel;
     }
 
-    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
-        return base.CreateDiscount(request, context);
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon == null)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request"));
+
+        dbContext.Coupons.Add(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation($"Coupon Discount Created Successfully {JsonSerializer.Serialize(coupon)}");
+        
+        var couponModel = coupon.Adapt<CouponModel>();
+
+        return couponModel;
+
     }
 
     public override Task<CouponModel> UdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
